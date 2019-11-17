04d1621c49f1535488dce66f1849814d36465f34
bonniepan02 <bonniepan02@gmail.com>
2019-11-17 00:46:26 -0500

    add mod rule

--- spec/shared_examples/rule_shared_examples.rb
+++ spec/shared_examples/rule_shared_examples.rb
@@ -1,13 +1,13 @@
 shared_examples 'rule' do
   context 'validating contract' do
     it 'should respond to get_string with_keywords num' do
-      expect(subject).to respond_to(:get_string).with_keywords(:num)
+      expect(rule).to respond_to(:get_string).with_keywords(:num)
     end
   end
 
   context '#get_string' do
     it 'should return nil if rule does not match' do
-      expect(subject.get_string(num: nil)).to eq(nil)
+      expect(rule.get_string(num: nil)).to eq(nil)
     end
   end
 end
